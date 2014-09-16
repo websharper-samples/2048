fefbdb49a09825e1de027eaaae3efc13c6462e7e
András Jankó <andras.janko@gmail.com>
2014-09-16 16:37:55 +0200

    fix build

--- tools/publish-site.ps1
+++ tools/publish-site.ps1
@@ -2,10 +2,7 @@
 rm -r build -errorAction ignore
 $d = mkdir build
 $d = mkdir build/html
-cp -r Site/Content build/html/
-cp -r Site/*.jpg build/html/
-cp -r Site/*.css build/html/
-cp -r Site/*.html build/html/
-cp -r Site/*.json build/html/
-
-
+cp -r Game2048/Content build/html/
+cp -r Game2048/*.html build/html/
+cp -r Game2048/style/*.css build/html/style/
+cp -r Game2048/style/fonts Game2048/style/
