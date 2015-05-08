8c36026fd519faf0e2c317a445b2d218532a6f2d
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-08 19:57:05 +0200

    AppVeyor-friendly build script

--- build.cmd
+++ build.cmd
@@ -3,11 +3,25 @@
 setlocal
 
 if not exist .paket\paket.exe (
-   .paket\paket.bootstrapper.exe
+  .paket\paket.bootstrapper.exe
 )
 
 .paket\paket.exe restore
 
+if "%MSBUILD_PATH%" == "" (
+  REM we're not deploying on Azure, set some variables
+  set MSBUILD_PATH="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
+  mkdir build
+  mkdir build\html
+  set DEPLOYMENT_TARGET=%CD%\build\html
+)
+
 %MSBUILD_PATH% /p:Configuration=Release
 
-xcopy /y /e Game2048 %DEPLOYMENT_TARGET%
+if %ERRORLEVEL% == 0 (
+  copy Game2048\*html %DEPLOYMENT_TARGET%
+  mkdir "%DEPLOYMENT_TARGET%\Content"
+  xcopy /y /e Game2048\Content "%DEPLOYMENT_TARGET%\Content"
+  mkdir "%DEPLOYMENT_TARGET%\style"
+  xcopy /y /e Game2048\style "%DEPLOYMENT_TARGET%\style"
+)
