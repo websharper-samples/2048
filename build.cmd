348b1690abf9814eb50b657920fbc52a3f357040
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-08 18:32:25 +0200

    Fix deployment path

--- build.cmd
+++ build.cmd
@@ -8,9 +8,6 @@ if not exist .paket\paket.exe (
 
 .paket\paket.exe restore
 
-set FSharpHome=%CD%\packages\FSharp.Compiler.Tools\tools
-set PATH=%FSharpHome%;%PATH%
-
 %MSBUILD_PATH% /p:Configuration=Release
 
-xcopy /y %DEPLOYMENT_SOURCE%\Game2048\ %DEPLOYMENT_TARGET%\
+xcopy /y Game2048 %DEPLOYMENT_TARGET%
