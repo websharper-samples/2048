4adb1031f054654caeb8a92d30a81dc424f049c6
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-08 18:14:30 +0200

    Build with msbuild, use paket for nuget

--- build.cmd
+++ build.cmd
@@ -1,9 +1,16 @@
 @ECHO OFF
 
 setlocal
-set FSharpHome=%CD%\tools\packages\FSharp.Compiler.Tools\tools
-set PATH=%PATH%;tools\NuGet
-set PATH=%PATH%;%FSharpHome%
-nuget install IntelliFactory.Build -nocache -pre -ExcludeVersion -o tools\packages
-nuget install FSharp.Compiler.Tools -nocache -pre -ExcludeVersion -o tools\packages
-fsi.exe --exec build.fsx %*
+
+if not exist .paket\paket.exe (
+   .paket\paket.bootstrapper.exe
+)
+
+.paket\paket.exe restore
+
+set FSharpHome=%CD%\packages\FSharp.Compiler.Tools\tools
+set PATH=%FSharpHome%;%PATH%
+
+%MSBUILD_PATH% /p:Configuration=Release
+
+xcopy /y %DEPLOYMENT_SOURCE%\Game2048\ %DEPLOYMENT_TARGET%\
